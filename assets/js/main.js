99949b7b9e6c25997e8cf3f2a96ac8008e800e6b
HamidzadeTural913 <tu76fe6rc@code.edu.az>
2022-05-20 13:23:38 +0400

    Task Added

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+let xhr = new XMLHttpRequest();
+console.log("test");
+
+fetch("https://jsonplaceholder.typicode.com/photos")
+  .then((resp) => resp.json())
+  .then((datas) => {
+    let container = document.querySelector(".container");
+    let row = document.createElement("div");
+    console.log(row);
+    console.log(container);
+    row.className = "row justify-content-between w-100 ms-2 text-center";
+
+    for (let i = 0; i < datas.length - 4900; i++) {
+      let card = document.createElement("div");
+      let img = document.createElement("img");
+      let img1 = document.createElement("img");
+      let card_body = document.createElement("div");
+      let h4 = document.createElement("h4");
+      let p = document.createElement("p");
+
+      var a = document.createElement("a");
+      var linkText = document.createTextNode("Info");
+      a.appendChild(linkText);
+      a.title = "Info";
+      a.href = `index.html?dataId=${datas[i].id}`;
+     console.log(datas[i].id);
+
+      card.className = "card w-50";
+      card_body.className = "card_body";
+      h4.className = "card-title mt-2";
+      p.className = "card-text";
+
+      img.className = "img";
+      p.className = "p";
+      a.className = "a";
+
+      img.src = datas[i].url;
+      img1.src = datas[i].thumbnailUrl;
+      p.innerText = datas[i].id;
+      h4.innerText = datas[i].title;
+
+      card_body.append(p);
+      card_body.appendChild(a);
+      card_body.append(h4);
+      card_body.append(img);
+      card_body.append(img1);
+
+      card.append(card_body);
+      row.append(card);
+
+        
+      }    
+    container.append(row);
+
+  });
